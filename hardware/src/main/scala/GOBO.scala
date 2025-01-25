e08348ebf73826fa8fad70b213a4351572096163
kongtiaochengtailang <1579924557@qq.com>
2025-01-25 08:56:27 +0800

    initial commit

---
+++ hardware/src/main/scala/GOBO.scala
@@ -0,0 +1,11 @@
+import spinal.core._
+
+object GOBO {
+  case class Config() {
+
+  }
+}
+
+class GOBO(cfg: GOBO.Config) extends Component {
+
+}
---
+++ hardware/src/main/scala/Olive.scala
@@ -0,0 +1,13 @@
+import spinal.core._
+
+object Olive {
+  case class Config() {
+
+  }
+}
+
+class Olive(cfg: Olive.Config) extends Component {
+  val io = new Bundle {
+    
+  }
+}
